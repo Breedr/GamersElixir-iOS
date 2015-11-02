722273eb051030a72f0390db93ff9681e2f8d358
Ed George <esm@hotmail.co.uk>
2015-11-02 08:30:35 +0000

    Add DetailViewController

---
+++ Gamers Elixir/Gamers Elixir/DetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  DetailViewController.h
+//  Gamers Elixir
+//
+//  Created by Ed George on 30/10/2015.
+//  Copyright © 2015 Ed George. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DetailViewController : UIViewController
+
+@end
--- Gamers Elixir/Gamers Elixir/ViewController.m
+++ Gamers Elixir/Gamers Elixir/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 #import "UIColor+CustomColors.h"
 #import "ArticleTableViewCell.h"
+#import "DetailViewController.h"
 
 
 @interface ViewController ()
@@ -18,12 +19,13 @@
 
 @implementation ViewController
 
+// MARK: - Lifecycle Methods
+
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.data = @[@"gaming news", @"articles", @"test me"];
     self.automaticallyAdjustsScrollViewInsets = NO;
     [self.tableView registerNib:[UINib nibWithNibName:@"ArticleTableViewCell" bundle:nil] forCellReuseIdentifier:@"articleCell"];
-    self.tableView.allowsSelection = NO;
 }
 
 - (void)viewWillAppear:(BOOL)animated{
@@ -37,14 +39,32 @@
     // Dispose of any resources that can be recreated.
 }
 
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    if ([[segue identifier] isEqualToString:@"showArticleDetailSegue"])
+    {
+        DetailViewController *vc = [segue destinationViewController];
+        // Pass any objects to the view controller here, like...
+        vc.title = @"Stub Story";
+    }
+}
+
+
+
+// MARK: - Table Methods
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
+    return 1;
+}
+
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
     return [self.data count];
 }
 
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
-    return 1;
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    [self performSegueWithIdentifier:@"showArticleDetailSegue" sender:nil];
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -54,6 +74,8 @@
     if(!cell){
         cell = [tableView dequeueReusableCellWithIdentifier:@"articleCell"];
     }
+    
+    cell.selectionStyle = UITableViewCellSelectionStyleNone;
     
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         UIImage * img = [UIImage imageWithData:[NSData dataWithContentsOfURL:[NSURL URLWithString:@"http://www.gamerselixir.com/wp-content/uploads/2015/10/el_ludicolo_bailando_disco_by_jacoblionheart-d3e3sof-672x372.png"]]];
@@ -77,6 +99,5 @@
 {
     return 220;
 }
-
 
 @end
